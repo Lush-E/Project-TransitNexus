f57c9202fd87d5779578583381f6ce6f39353be8
Lush_E <akhr1104@gmail.com>
2026-03-16 22:39:54 +0900

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,40 +1,38 @@
 @echo off
 setlocal
-cd /d "%~dp0"
-echo.
-echo === TransitNexus Launcher ===
-echo [1] Start app
-echo [2] Update and start
-echo [3] Install dependencies only
-echo [4] Exit
-echo.
 
-set ACTION=
-choice /c 1234 /n /m "Choose 1-4: "
-if errorlevel 4 goto :end
-if errorlevel 3 goto :actionInstall
-if errorlevel 2 goto :actionUpdateStart
-if errorlevel 1 goto :actionStart
+set "BASE_DIR=%~dp0"
+set "PKG=%BASE_DIR%package.json"
+set "NODE_MODULES=%BASE_DIR%node_modules"
 
-:actionStart
-set ACTION=start
-goto :run
-
-:actionUpdateStart
-set ACTION=update-start
-goto :run
-
-:actionInstall
-set ACTION=install
-goto :run
-
-:run
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0launcher.ps1" -Action %ACTION%
-if errorlevel 1 (
-  echo.
-  echo Launcher exited with an error.
+if not exist "%PKG%" (
+  echo [ERROR] package.json not found: %PKG%
   pause
+  exit /b 1
 )
 
-:end
-endlocal
+if not exist "%NODE_MODULES%" (
+  echo Installing dependencies...
+  pushd "%BASE_DIR%"
+  call npm install
+  if errorlevel 1 (
+    echo [ERROR] npm install failed.
+    popd
+    pause
+    exit /b 1
+  )
+  popd
+)
+
+pushd "%BASE_DIR%"
+call npm run start
+set "EXIT_CODE=%ERRORLEVEL%"
+popd
+
+if not "%EXIT_CODE%"=="0" (
+  echo [ERROR] App exited with code %EXIT_CODE%.
+  pause
+  exit /b %EXIT_CODE%
+)
+
+exit /b 0
